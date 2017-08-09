d3be23a4669846ad943a194a3ce67c28867b35c2
Sunil Gogavi -X (sgogavi - TATA CONSULTANCY SERVICES LIMITED at Cisco) <sgogavi@partnet.cisco.com>
2017-08-09 12:50:01 +0530

    Helloworld files added

---
+++ Helloworld.java
@@ -0,0 +1,14 @@
+import java.io.*;
+public class Helloworld
+{
+	public static void main(String[] args)
+	{
+		for(int i=0;i<10;i++)
+		{
+			System.out.println("Hello World...."+i);
+			
+		}
+		
+	}
+	
+}
